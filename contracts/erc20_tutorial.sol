43a14444ae33badf48d93d2bb050169345424336
OITransfer <54826945+OITransfer@users.noreply.github.com>
2019-09-11 00:04:22 +0200

    Contract.sol
    New own

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'OIT' token contract
 //
 // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Symbol      : OIT
+// Name        : OIT
 // Total supply: 100000000
 // Decimals    : 18
 //
